1206d358d3aefd3f319f9173c25e340e0a4a908a
poorna.sd9@gmail.com <mahesh506@sasi.ac.in>
2021-05-07 19:58:24 +0530

    Updates

---
+++ precache-manifest.4d1d60af643fdc19e8a0bf990c0a53ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcad1b39b5ed3fcae41e458bb114988",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "cb35f68cfada641d8872",
+    "url": "/covid-tracker/static/css/main.3ae39ab4.chunk.css"
+  },
+  {
+    "revision": "38bc5635522c26e3d875",
+    "url": "/covid-tracker/static/js/2.3a3ed2c1.chunk.js"
+  },
+  {
+    "revision": "9db836fc7371514a258dc1b02075d61d",
+    "url": "/covid-tracker/static/js/2.3a3ed2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb35f68cfada641d8872",
+    "url": "/covid-tracker/static/js/main.688e135e.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
